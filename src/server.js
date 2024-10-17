52b74813e8591645094697677caa5dd8a1c9a345
Mughees <mughees.qasim.mq@gmail.com>
2024-10-18 00:54:26 +0300

    Add client port in cors

--- src/server.js
+++ src/server.js
@@ -4,22 +4,26 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const PORT = process.env.PORT === undefined ? 3000 : process.env.PORT;
+const SERVER_PORT = process.env.SERVER_PORT === undefined ? 3000 : process.env.SERVER_PORT;
+const CLIENT_PORT = process.env.CLIENT_PORT === undefined ? 3001 : process.env.CLIENT_PORT;
 
 const httpServer = createServer();
 
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:" + PORT
+        origin: "http://localhost:" + CLIENT_PORT
       }
 });
 
+let clients = [];
+
 io.on('connection', (socket) => {
-    socket.send('Hello World');
+    clients.add()
+    console.log("Client Connected");
 
     socket.on('message', () => {
         console.log(message);
     });
 });
 
-httpServer.listen(PORT);
+httpServer.listen(SERVER_PORT);
